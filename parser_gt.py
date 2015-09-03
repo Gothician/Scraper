633016b6761724016100215820e8ff82f2b614d1
Gothician <gothicianalter@gmail.com>
2015-09-03 17:20:30 +0300

    Added parser_olx

--- parser_gt.py
+++ parser_gt.py
@@ -4,13 +4,12 @@ Created on Sat Aug 29 16:31:26 2015
 
 @author: gothician
 """
-import urllib # подключили библиотеку urllib
+import urllib3 # подключили библиотеку urllib
 import lxml.html # подключили библиотеку lxml
 # Открываем ГТ
-page = urllib.request.urlopen("http://geektimes.ru/all")
+page = urllib3.request.urlopen("http://geektimes.ru/all")
 # Получаем HTML-код главной страницы
 doc = lxml.html.document_fromstring(page.read())
-print(doc)
 # Выбираем посты
 for post in doc.cssselect('div.post'):
     print('Пост:')
@@ -18,11 +17,18 @@ for post in doc.cssselect('div.post'):
     print(post.find_class('post_title')[0].text)
     # выводим на экран текст до хабраката.
     print('Текст:')
-    print(post.cssselect('div.content')[0].text)
+    post_div = post.cssselect('div.content')[0]
+    text = (post_div.text)
+    for children in post_div.getchildren():
+        if children.text != None:
+            text += children.text
+        if children.tail != None:
+            text += children.tail
+    print(text.strip())
     # выводим на экран оценку поста.
     print('Оценка: %s' % (post.find_class('score')[0].text))
     # выводим на экран автора и его рейтинг.
     rate = post.find_class('rating')
     print('Автор <%s> с рейтингом <%s>' %(rate[0].getprevious().text, rate[0].text))
-    print(post.cssselect('div.content')[1].attrib)
+    print()
 
